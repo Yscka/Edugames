435e7ba4db74b2418260b892a8a035353cfb1b71
yska <lagache.dimitri59@gmail.com>
2018-01-19 14:59:41 +0100

    creation des entités Class, éleves

---
+++ src/EG/ClassBundle/Entity/Pupil.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace EG\ClassBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Pupil
+ *
+ * @ORM\Table(name="pupil")
+ * @ORM\Entity(repositoryClass="EG\ClassBundle\Repository\PupilRepository")
+ */
+class Pupil
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(name="lastname", type="string", length=255)
+     */
+    private $lastname;
+
+    /**
+     * @ORM\Column(name="birthdate", type="datetime")
+     */
+    private $birthdate;
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Pupil
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set lastname
+     *
+     * @param string $lastname
+     *
+     * @return Pupil
+     */
+    public function setLastname($lastname)
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    /**
+     * Get lastname
+     *
+     * @return string
+     */
+    public function getLastname()
+    {
+        return $this->lastname;
+    }
+
+    /**
+     * Set birthdate
+     *
+     * @param \DateTime $birthdate
+     *
+     * @return Pupil
+     */
+    public function setBirthdate($birthdate)
+    {
+        $this->birthdate = $birthdate;
+
+        return $this;
+    }
+
+    /**
+     * Get birthdate
+     *
+     * @return \DateTime
+     */
+    public function getBirthdate()
+    {
+        return $this->birthdate;
+    }
+}
