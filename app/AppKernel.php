6ed9d3b2518678de7ee9a5c279ba792b59d7c4e9
yska <lagache.dimitri59@gmail.com>
2018-01-24 09:50:23 +0100

    Création GameBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new EG\CoreBundle\EGCoreBundle(),
             new EG\ViewClassBundle\EGViewClassBundle(),
+            new EG\GameBundle\EGGameBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
